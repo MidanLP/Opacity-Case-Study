d9dcdb10b7c2f6cea54ff5bf87cf677b3731d545
Nadim <nadim.abuhaltam@gmail.com>
2025-08-02 21:41:17 +0200

    forgot last upload of code comments

--- Testing/createBoxplot.py
+++ Testing/createBoxplot.py
@@ -1,23 +1,23 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 
-df1 = pd.read_excel("time_output.xlsx", sheet_name="Server, NoCache")
+df1 = pd.read_excel("time_output.xlsx", sheet_name="Testing Timings")
 
-df2 = pd.read_excel("time_output.xlsx", sheet_name="Server, Cache")
+#df2 = pd.read_excel("time_output.xlsx", sheet_name="Server, Cache")
 
 #df3 = pd.read_excel("time_data.xlsx", sheet_name="Sheet1")
 
 plt.figure(figsize=(8, 6))
 
-plt.boxplot(df1["Time"].dropna(), vert=True, patch_artist=True, positions=[1], widths=0.6, labels=["server, no Cache"])
+plt.boxplot(df1["Time"].dropna(), vert=True, patch_artist=True, positions=[1], widths=0.6, labels=["normal Time"])
 
-plt.boxplot(df2["Time"].dropna(), vert=True, patch_artist=True, positions=[2], widths=0.6, labels=["server, Cache"])
+plt.boxplot(df1["Bust"].dropna(), vert=True, patch_artist=True, positions=[2], widths=0.6, labels=["bust Time"])
 
 #plt.boxplot(df3["Time_Value"].dropna(), vert=True, patch_artist=True, positions=[3], widths=0.6, labels=["Server, cache"])
 
-plt.title("Comparison")
+plt.title("Timing result")
 plt.ylabel("Time Value in Ms")
 #plt.xticks([1, 2, 3], ['local, cache','local, no cache', 'Server, cache'])
 
-plt.savefig("boxplot_comparison.png")
+plt.savefig("CacheVsBust.png") #name of file to be created
 plt.show()
